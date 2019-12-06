834b06c9d84cb06a5786ee2fabb8f2655c2f70f5
Ty Bolchoz <tybolt@gmail.com>
2019-12-06 15:22:48 -0500

    Initial Commit

---
+++ Tree-iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tree-iOS
+//
+//  Created by Ty Bolchoz on 12/6/19.
+//  Copyright © 2019 Ty Bolchoz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tree-iOSTests/Tree_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tree_iOSTests.swift
+//  Tree-iOSTests
+//
+//  Created by Ty Bolchoz on 12/6/19.
+//  Copyright © 2019 Ty Bolchoz. All rights reserved.
+//
+
+import XCTest
+@testable import Tree_iOS
+
+class Tree_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
