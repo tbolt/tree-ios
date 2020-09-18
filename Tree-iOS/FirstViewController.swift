6a8f76d3c4442e46f4f182ae054c4da91c4dff26
Ty Bolchoz <tybolt@gmail.com>
2020-09-17 21:27:11 -0400

    catch up, update build settings

--- Tree-iOS/FirstViewController.swift
+++ Tree-iOS/FirstViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import Alamofire
 
 class FirstViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var textBlock: UITextView?
+  @IBOutlet weak var toggleText: UIButton?
+  
+  var myText = "bahhhhhh this is my text."
+  var myAltText = "this is some other text now...."
+  
+  func updateTextBlock(count:Int) {
+    print("made it to updateTextBlock")
+    let newString = String(count)
+    textBlock?.text = newString
+  }
+  
+  @IBAction func updateTextButtonPressed(_ sender: Any) {
+    print("button triggered....")
+//    textBlock?.text = myAltText
+    
+    AF.request("https://swapi.co/api/films/").responseJSON { response in
+      
+      switch response.result {
+      case .success(let value as [String: Any]):
+          print("value of count: \(value["count"] ?? 0)")
+          self.updateTextBlock(count: value["count"] as! Int)
+//          for (key, val) in value {
+//              print("The value to '\(key)' is '\(val)'.")
+//          }
+      case .failure(let error):
+          print("failure: \(error)")
+      default:
+          fatalError("received non-dictionary JSON response")
+      }
     }
-
-
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
 }
-
---
+++ Tree-iOS/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  Tree-iOS
+//
+//  Created by Ty Bolchoz on 12/6/19.
+//  Copyright © 2019 Ty Bolchoz. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("heyo!")
+
+    }
+
+
+}
+
